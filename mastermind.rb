52911a4ad063301349754955a4481917a2f59f07
Rafael <mikudesudesu@vivaldi.net>
2025-01-30 21:16:03 -0300

    - bugchasing version - chasing the a core math problem

--- mastermind.rb
+++ mastermind.rb
@@ -30,7 +30,7 @@ def computer_code_generator
   Array.new(4) { rand(1..6) }
 end
 
-SEQUENCE = [1..6]
+SEQUENCE = [1, 2, 3, 4, 5, 6]
 
 def game_start
   print 'type the color sequence'
@@ -39,19 +39,20 @@ end
 
 def game_intro
   print "Welcome to Mastermind \n the computer will use one of 6 colors to represent it's code\n "
-  SEQUENCE.each { |n| print color_squares(n) }
-  print "\nfor each correct guess in the correct position, you get a \e[40m \e[0m\n"
-  print "for each correct guess in the incorrect position, you get a \e[47m \e[0m\n"
+  #binding.pry
+  display_code(SEQUENCE)
+  print "\nfor each correct guess in the correct position, you get a \e[37m ⬤\e[0m\n"
+  print "for each correct guess in the incorrect position, you get a \e[30m ⬤\e[0m\n"
   print "the computer will generate a 4 color code, and you have to guess within 12 turns\n"
 end
 
-def play_round
+def play_round(round_number)
   guess = false
   until guess
     print "Guess the colors\n"
     guess = gets.chomp
     if valid_input?(guess) == true
-      print("Round played successfully\n")
+      print("Round #{round_number} played successfully\n")
     else
       print("Your answer should only contain numbers and only have 4 digits\n")
     end
@@ -83,8 +84,8 @@ end
 
 def show_score(score)
   if score.instance_of?(Array)
-    score[0].times { print "\e[37m ⬤\e[0m" }
-    score[1].times { print "\e[30m ⬤\e[0m" }
+    score[0].times { print "\e[37m ⬤\e[0m  " }
+    score[1].times { print "\e[30m ⬤\e[0m  " }
   else
     'Game end'
   end
@@ -95,16 +96,37 @@ def play_game(rounds = 0)
   print computer_code = computer_code_generator
   while rounds <= 12
     rounds += 1
-    player_guess = play_round.to_i.digits.reverse
+    player_guess = play_round(rounds).to_i.digits.reverse
     score = calculate_score(computer_code, player_guess)
     if score.instance_of?(String)
       print(show_score([4, 0]))
       rounds = 12
       break
     else
-      print(show_score(score))
+      print("xdddddd #{show_score(score)}\n")
     end
   end
 end
 
+#play_game
+print(color_squares(computer_code_generator))
+
+def display_code(code)
+  code.each { |pin| print color_squares(pin) }
+end
+
 play_game
+
+# a funcao color_squares cospe seu quadrado colorido com numero. o computer code generator e so 4 digitos aleatorios
+# correspondente ao codigo. voce pode criar uma funcao que use os dois para fazer display das opcoes selecionadas (X)
+
+# 5142
+# 1111
+
+# proximo passos
+# Arrumar quando se pula linha
+# Conferir se o jogo funciona corretamente
+# Mexa no gerador pra gerar sempre o mesmo numero e teste os uso-caso
+# Adicione as correlacoes de numeros com cores
+# Altere o inicio pra sair daquele quadrado preto esquisito que voce nao tava conseguindo mudar pras novas bolas
+# coloridas
